e3570d3e5c74235ec8d1a1c6d7b5944ba86d9f61
Kent Loofbourrow <kent.loofbourrow@loofmail.com>
2024-04-03 18:30:55 -0400

    Add USB identification for Loof Home Automations Crackbone Gamepad

--- boot.py
+++ boot.py
@@ -1,5 +1,8 @@
 import usb_hid
 from joystick_xl.hid import create_joystick
+import supervisor
+
+supervisor.set_usb_identification("Loof Home Automations", "Crackbone Gamepad", 0x1209, 0x6660)
 
 usb_hid.disable()
 
